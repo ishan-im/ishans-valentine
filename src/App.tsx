bd75e1dd40223a0e525112641ba90d4d1c570a31
ishan-im <ishanmondal713127@gmail.com>
2025-02-04 10:07:30 +0530

    for valentine

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { useState,useEffect } from "react";
+import useSound from 'use-sound';
+
+import withYou from '../src/assets/withYou.mp3'
+
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const [play,{stop}] = useSound(withYou);
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  useEffect(() => {
+    // Automatically play sound when the component is mounted
+    play();
+
+    // Cleanup function to stop sound when the component is unmounted
+    return () => {
+      stop();
+    };
+  }, [play, stop]);
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "What if I asked really nicely?",
+      "Pretty please",
+      "With a chocolate rice cake on top",
+      "What about a matcha frostie",
+      "PLEASE POOKIE",
+      "But :*(",
+      "I am going to die",
+      "Yep im dead",
+      "please darlin",
+      'please babe',
+      'Samjho na :(',
+      ":((((",
+      "PRETTY PLEASE",
+      "Estoy muerto",
+      "No :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center" onMouseEnter={() => play()} onMouseLeave={() => stop()}>
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
